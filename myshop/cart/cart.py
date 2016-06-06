1b99618140ebc9e80f50a42fa2e3090fa0d8eee0
lafabo <lazyfatboy@ya.ru>
2016-06-07 01:09:25 +0400

    day 182

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -14,7 +14,8 @@ class Cart(object):
 
 	def add(self, product, quantity=1, update_quantity=False):
 		product_id = str(product.id)
-		if product_id and self.cart:
+
+		if product_id not in self.cart:
 			self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
 
 		if update_quantity:
@@ -49,7 +50,7 @@ class Cart(object):
 		return sum(item['quantity'] for item in self.cart.values())
 
 	def get_total_price(self):
-		return sum(Decimal(item['price'] * item['quantity'] for item in self.cart.values()))
+		return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
 
 	def clear(self):
 		del self.session[settings.CART_SESSION_ID]

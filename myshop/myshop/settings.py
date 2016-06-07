d13b0ecb555b961051a67b911ce896a3ef46cc96
lafabo <lazyfatboy@ya.ru>
2016-06-08 01:55:37 +0400

    day 183

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -110,3 +110,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,8 +1,46 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+import csv
+import datetime
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 
 
 # Register your models here.
+def export_to_csv(modeladmin, request, queryset):
+	opts = modeladmin.model._meta
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename=%s.csv' % opts.verbose_name
+	writer = csv.writer(response)
+
+	fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+	writer.writerow([field.verbose_name for field in fields])
+
+	for obj in queryset:
+		data_row = []
+		for field in fields:
+			value = getattr(obj, field.name)
+			if isinstance(value, datetime.datetime):
+				value = value.strftime('%d/%m/%Y')
+			data_row.append(value)
+		writer.writerow(data_row)
+	return response
+
+export_to_csv.short_description = 'Export to CSV'
+
+
+def order_detail(obj):
+	return '<a href="%s">View</a>' % reverse('orders:admin_order_detail', args=[obj.id])
+
+order_detail.allow_tags = True
+
+
+def order_pdf(obj):
+	return '<a href="%s">PDF</a>' % reverse('orders:admin_order_pdf', args=[obj.id])
+
+order_pdf.allow_tags = True
+order_pdf.short_description = 'PDF bill'
+
 class OrderItemInline(admin.TabularInline):
 	model = OrderItem
 	raw_id_fields = ['product']
@@ -10,9 +48,10 @@ class OrderItemInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
 	list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-	                'postal_code', 'city', 'paid', 'created', 'updated']
+	                'postal_code', 'city', 'paid', 'created', 'updated', order_detail, order_pdf]
 	list_filter = ['paid', 'created', 'updated']
 	inlines = [OrderItemInline]
+	actions = [export_to_csv]
 
 
 admin.site.register(Order, OrderAdmin)

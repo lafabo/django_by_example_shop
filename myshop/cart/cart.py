dd807a11fd9ec6c906e99ed3dc3970334ef5a287
lafabo <lazyfatboy@ya.ru>
2016-06-09 01:52:45 +0400

    day 184

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart(object):
@@ -9,6 +10,8 @@ class Cart(object):
 		cart = self.session.get(settings.CART_SESSION_ID)
 		if not cart:
 			cart = self.session[settings.CART_SESSION_ID] = {}
+
+		self.coupon_id = self.session.get('coupon_id')
 
 		self.cart = cart
 
@@ -55,3 +58,16 @@ class Cart(object):
 	def clear(self):
 		del self.session[settings.CART_SESSION_ID]
 		self.session.modified = True
+
+	def coupon(self):
+		if self.coupon_id:
+			return Coupon.objects.get(id=self.coupon_id)
+		return None
+
+	def get_discount(self):
+		if self.coupon:
+			return (self.coupon.discount / Decimal('100')) * self.get_total_price()
+		return Decimal('0')
+
+	def get_total_price_after_discount(self):
+		return self.get_total_price() - self.get_discount()
--- myshop/coupons/admin.py
+++ myshop/coupons/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Coupon
+
 
 # Register your models here.
+class CouponAdmin(admin.ModelAdmin):
+	list_display = ['code', 'valide_from', 'valide_to', 'discount', 'active']
+	list_filter = ['active', 'valide_from', 'valide_to']
+	search_fields = ['code']
+
+admin.site.register(Coupon, CouponAdmin)

4e4414d94f3cd82f08a8d0a9ea26fc4b09c6100e
lafabo <lazyfatboy@ya.ru>
2016-06-10 21:31:46 +0400

    day 185

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from shop.models import Product
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
+from coupons.models import Coupon
 
 
 # Create your models here.
@@ -13,6 +16,8 @@ class Order(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 	paid = models.BooleanField(default=False)
+	coupon = models.ForeignKey(Coupon, related_name='orders', null=True, blank=True)
+	discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
 	class Meta:
 		ordering = ('-created',)
